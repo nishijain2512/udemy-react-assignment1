5e867b0335544b6c4a9582519affaa67dccb1138
Nishi Jain <nishijain@nishis-mbp.attlocal.net>
2019-03-30 16:31:27 -0400

    1st commit

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,9 @@
+import React from React;
+
+const userInput = (props) => {
+    return(
+        <div className='userInput'>
+
+        </div>
+    )
+};
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,10 @@
+import React from React;
+
+const userOutput = (props) => {
+    return(
+        <div className='userOutput'>
+            <p>I am paragraph ONE</p>
+            <p>I am paragraph TWO</p>
+        </div>
+    )
+};
